529a5c2b457a1083fd4a88e60814d611ebd61d87
Munirat <muniratolayiwola@gmail.com>
2022-06-17 00:45:03 +0100

    created models and added category and product views

--- api/models.py
+++ api/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    imageUrl = models.URLField(null=True)
+    status = models.BooleanField(default=True)
+    date_created = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return self.name
+
+
+class Cart(models.Model):
+    cart_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    products = models.ManyToManyField(Product)
+    total_price=models.IntegerField()
+
+    class Meta:
+        ordering = ['cart_id', '-created_at']
+        
+
+    def __str__(self):
+        return f'{self.cart_id}'
+    
+
+class Payment(models.Model):
+    cart_id= models.ForeignKey(Cart, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    invoice= models.CharField(max_length=400)
+    payment_confirmed=models.BooleanField(default=False)
+    payment_hash=models.CharField(max_length=300)
