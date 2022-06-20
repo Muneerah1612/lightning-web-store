bd91bc9c634e3921936d4b1edd13e4d3218f8bb1
Munirat <muniratolayiwola@gmail.com>
2022-06-21 00:03:01 +0100

    created cart endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ProductListView, AddProductView, CreateCategoryView
+from .views import ProductListView, AddProductView, CreateCategoryView, CreateCartItemView, CartSummaryView
 
 urlpatterns=[
     path('category/', CreateCategoryView.as_view(), name='category'),
     path('addproduct/',AddProductView.as_view(),name='product'),
     path('product_list/', ProductListView.as_view(), name='product_list'),
+    path('add_to_cart/', CreateCartItemView.as_view(), name='cart_item'),
+    path('cart_price/', CartSummaryView.as_view(), name='cart_price'),
 ]
